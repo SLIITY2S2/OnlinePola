57229c5e9662ed139508b73edf08489793baa345
LankaniDeSilva <IT20221850@my.sliit.lk>
2021-08-20 19:16:53 +0530

    Create Spinner in home fragment

--- app/src/main/java/com/madd/mobileapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/madd/mobileapp/ui/home/HomeFragment.java
@@ -1,18 +1,19 @@
 package com.madd.mobileapp.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -23,8 +24,7 @@ public class HomeFragment extends Fragment{
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
-
-
+    Spinner dropdown;
 
     public HomeFragment(){
 
@@ -38,6 +38,10 @@ public class HomeFragment extends Fragment{
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        dropdown = root.findViewById(R.id.cat_spinner);
+        initspinnerfooter();
+
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -49,6 +53,28 @@ public class HomeFragment extends Fragment{
 
         return root;
     }
+    private void initspinnerfooter() {
+        String[] items = new String[]{
+                "Vegetables", "Fruits", "Animal Product",
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
